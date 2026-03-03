5c26738a8914a2590da6559a53a3f3f4d8a53463
Ravi Der <derravi00@gmail.com>
2026-03-03 22:53:23 +0530

    Add Pydantic model file

---
+++ schema/pydantic_model.py
@@ -0,0 +1,13 @@
+
+from pydantic import BaseModel,Field
+from typing import Annotated
+from pydantic import model_validator
+
+class gpt_model(BaseModel):
+    input_text:Annotated[str,Field(...,examples=["How can i Assist you?"])]
+
+    @model_validator(mode='after')
+    def text_checker(cls,model):
+        model.input_text = " ".join(model.input_text.split())
+
+        return model
