f4cd66588a2bc43d15c3c7001c5cc50404cf10e2
Ravi Der <derravi00@gmail.com>
2026-03-09 23:02:03 +0530

    Upatef Code

--- Data_base/db_model.py
+++ Data_base/db_model.py
@@ -1,12 +1,21 @@
-from sqlalchemy import Column, Integer, Text, DateTime
+from sqlalchemy import Column, Integer, Text, DateTime, ForeignKey
 from datetime import datetime
 from .db_engine import Base
+
+
+class chat_session(Base):
+    __tablename__ = "chat_sessions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(Text)
+    created_at = Column(DateTime, default=datetime.utcnow)
 
 
 class gpt_model(Base):
     __tablename__ = "gpt_prompts"
 
     id = Column(Integer, primary_key=True, index=True)
+    session_id = Column(Integer, ForeignKey("chat_sessions.id"))
     original_text = Column(Text)
     answer_text = Column(Text)
     time_stemp = Column(DateTime, default=datetime.utcnow)
