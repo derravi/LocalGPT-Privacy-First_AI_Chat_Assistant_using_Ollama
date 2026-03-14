bfb89696fcb68288f1b69630ad42acc5561ffb23
Ravi Der <derravi00@gmail.com>
2026-03-14 23:52:21 +0530

    Updated Code

--- schema/pydantic_model.py
+++ schema/pydantic_model.py
@@ -1,10 +1,14 @@
 from pydantic import BaseModel, Field
 from typing import List
 
+try:
 
-class gpt_pydantic_model(BaseModel):
+    class gpt_pydantic_model(BaseModel):
 
-    input_text: List[str] = Field(
-        ...,
-        examples=[["How can I assist you?", "How are you?"]]
-    )
+        input_text: List[str] = Field(
+            ...,
+            examples=[["How can I assist you?", "How are you?"]]
+        )
+
+except Exception as e:
+    print(f"Error:{e}.")
