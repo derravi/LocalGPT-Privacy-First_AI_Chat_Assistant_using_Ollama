bba83d64a81254cec6102c93560654e630b3079b
Ravi Der <derravi00@gmail.com>
2026-03-14 22:28:34 +0530

    Add Frontend code of the Streanlit

--- frontend.py
+++ frontend.py
@@ -1,35 +1,72 @@
-import streamlit as st
+import streamlit as st 
 import requests
 
 API_URL = "http://127.0.0.1:9000"
 
-st.title("LocalGPT Chat")
+st.title("Local GPT Chat")
 
-#Session ID
-session_id = st.number_input("Session ID",min_value=1,step=1)
+# ==============================
+#Session Create
+# ==============================
 
-#prompt input
-user_prompt = st.text_area("Enter Your Prompt here...")
+st.sidebar.header("Create Chat Session")
 
-if st.button("Send"):
-    payload = {
-        "input_text" : [user_prompt],
-    }
+session_title = st.sidebar.text_input("Session Title")
+
+if st.sidebar.button("Create Session"):
 
     response = requests.post(
-        f"{API_URL}/answer?session_id={session_id}",
-        json=payload
+        f"{API_URL}/create_session",
+        params = {"title":session_title}
     )
 
-    if response.status_code == 200:
+    data = response.json()
 
+    st.session_state["session_id"] = data["session_id"]
+
+    st.sidebar.success(f"Session Created: {data['session_id']}")
+
+# ==============================
+#Chat Section code
+# ==============================
+
+st.header("Ask Question")
+
+prompt = st.text_input("Enter your prompt")
+
+if st.button("Send Prompt"):
+    if "session_id" not in st.session_state:
+        st.error("Please create a session first!")
+    else:
+        response = requests.post(
+            f"{API_URL}/answer",
+            params ={"session_id":st.session_state["session_id"]},
+            json = {
+                "input_text":[prompt]
+            }
+        )
         data = response.json()
 
-        for i in data["responses"]:
-            st.write("### Prompt")
-            st.write(i["Prompt"])
+        answer = data["responses"][0]["answer"]
 
-            st.write("### Answer")
-            st.write(r["Answer"])
-    else:
-        st.error("Error connecting to API.")
+        st.subheader("Answer")
+        st.write(answer)
+
+# ==============================
+#History Code
+# ==============================
+
+st.header("Chat History")
+
+if st.button("Load History"):
+
+    response = requests.get(f"{API_URL}/history")
+    data = response.json()
+
+    for i in data:
+        st.write("Prompt:",i["original_text"])
+        st.write("Answer:",i["answer_text"])
+        st.write("Prompt:",i["date_and_time"])
+        st.write("----")
+
+        
